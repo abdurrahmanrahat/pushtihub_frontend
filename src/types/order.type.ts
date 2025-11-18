d39569ceb293239a8b3c0d8e20f271fc4961a535
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-11-18 16:19:05 +0600

    order type

--- src/types/order.type.ts
+++ src/types/order.type.ts
@@ -1,26 +1,52 @@
+import { TSelectedVariant } from "./cart.type";
 import { TProduct } from "./product.type";
+
+export type TCustomerInfo = {
+  fullName: string;
+  phone: string;
+  fullAddress: string;
+  country: string;
+  orderNotes?: string;
+};
 
 export type TOrderItem = {
   _id: string;
   product: TProduct;
   quantity: number;
+
+  selectedVariants: TSelectedVariant[];
+
+  unitSellingPrice: number | null;
+  unitPrice: number | null;
+  lineTotal: number;
+};
+
+export type TPaymentDetails = {
+  method: "bkash" | "nagad";
+  phone: string;
+  transactionId: string;
 };
 
 export type TOrder = {
   _id: string;
   orderNumber: string;
-  fullName: string;
-  fullAddress: string;
-  phone: string;
-  country: string;
-  orderNotes?: string;
+
+  customerInfo: TCustomerInfo;
+
   shippingOption: "dhaka" | "outside";
+  shippingCost: number;
+
   orderItems: TOrderItem[];
+
   subtotal: number;
   total: number;
+
+  paymentDetails: TPaymentDetails;
+
   status: "pending" | "processing" | "shipped" | "delivered" | "cancelled";
-  paymentMethod: string;
+
   isDeleted: boolean;
+
   createdAt: string;
   updatedAt: string;
   __v: number;
